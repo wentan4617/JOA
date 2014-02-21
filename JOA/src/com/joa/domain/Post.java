2d0b43dcf5a82fb1889822875fb2960e7ddaf480
wentan4617 <wentan4617@sina.com>
2014-02-21 17:04:08 +0800

    新增setget方法

--- JOA/src/com/joa/domain/Post.java
+++ JOA/src/com/joa/domain/Post.java
@@ -1,6 +1,7 @@
 package com.joa.domain;
 
 import java.io.Serializable;
+import java.util.Set;
 
 public class Post implements Serializable {
 	private Long pid;
@@ -8,5 +9,39 @@ public class Post implements Serializable {
 	private String description;
 	
 	//岗位和user是多对多关系
+	private Set<User> users;
+
+	public Long getPid() {
+		return pid;
+	}
+
+	public void setPid(Long pid) {
+		this.pid = pid;
+	}
+
+	public String getPname() {
+		return pname;
+	}
+
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Set<User> getUsers() {
+		return users;
+	}
+
+	public void setUsers(Set<User> users) {
+		this.users = users;
+	}
+	
 	
 }
--- JOA/src/com/joa/domain/User.java
+++ JOA/src/com/joa/domain/User.java
@@ -1,6 +1,7 @@
 package com.joa.domain;
 
 import java.io.Serializable;
+import java.util.Set;
 
 public class User implements Serializable {
 	private Long uid;
@@ -10,7 +11,49 @@ public class User implements Serializable {
 	private String phone;
 	
 	//用户与post是多对多关系
-	
+	private Set<Post> posts;
 	//用户与department是多对一关系
-	
+	private Department department;
+	public Long getUid() {
+		return uid;
+	}
+	public void setUid(Long uid) {
+		this.uid = uid;
+	}
+	public String getUname() {
+		return uname;
+	}
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public Set<Post> getPosts() {
+		return posts;
+	}
+	public void setPosts(Set<Post> posts) {
+		this.posts = posts;
+	}
+	public Department getDepartment() {
+		return department;
+	}
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
 }
