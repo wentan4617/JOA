8c9900faf118cde88960a00eb7c40f6b5985d8d5
wentan4617 <wentan4617@sina.com>
2014-04-27 18:49:16 +0800

    test commit with git gui
    Signed-off-by: wentan4617 <wentan4617@sina.com>

--- JOA/src/com/joa/action/PersonAction.java
+++ JOA/src/com/joa/action/PersonAction.java
@@ -8,7 +8,7 @@ import com.opensymphony.xwork2.ActionSupport;
 public class PersonAction extends ActionSupport {
 	
 	private PersonService personService;
-
+	//只是增加一个变更。测试一下git gui
 	public PersonService getPersonService() {
 		return personService;
 	}
